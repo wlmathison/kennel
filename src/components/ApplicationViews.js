4b9004af5a203d799ac1d7ec8fc60a831bd91a3d
Billy Mathison <wlmathison@gmail.com>
2019-04-16 16:03:54 -0500

    inputting all information from the chapter

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,6 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
+import { withRouter } from 'react-router'
 import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
@@ -8,6 +9,7 @@ import AnimalManager from "../modules/AnimalManager"
 import LocationManager from "../modules/LocationManager"
 import EmployeeManager from "../modules/EmployeeManager"
 import OwnerManager from "../modules/OwnerManager"
+import AnimalDetail from "./animal/AnimalDetail"
 
 class ApplicationViews extends Component {
     state = {
@@ -19,7 +21,7 @@ class ApplicationViews extends Component {
 
     componentDidMount() {
         const newState = {}
-    
+
         LocationManager.getAll()
             .then(locations => newState.locations = locations)
             .then(() => AnimalManager.getAll())
@@ -31,20 +33,19 @@ class ApplicationViews extends Component {
             .then(() => this.setState(newState))
     }
 
-    deleteAnimal = id => {
-        AnimalManager.removeAndList(id)
-            .then(animals => this.setState({
-                animals: animals
-            })
-        )
-    }
+    deleteAnimal = id => AnimalManager.delete(id)
+    .then(AnimalManager.getAll)
+    .then(animals => {
+        this.props.history.push("/animals")
+        this.setState({ animals: animals })
+    })
 
     deleteEmployee = id => {
         EmployeeManager.removeAndList(id)
             .then(employees => this.setState({
                 employees: employees
             })
-        )
+            )
     }
 
     deleteOwner = id => {
@@ -52,7 +53,7 @@ class ApplicationViews extends Component {
             .then(owners => this.setState({
                 owners: owners
             })
-        )
+            )
     }
 
     render() {
@@ -63,7 +64,7 @@ class ApplicationViews extends Component {
                 }} />
                 <Route exact path="/animals" render={() => {
                     return <AnimalList deleteAnimal={this.deleteAnimal}
-                                    animals={this.state.animals} />
+                        animals={this.state.animals} />
                 }} />
                 <Route path="/employees" render={(props) => {
                     return <EmployeeList deleteEmployee={this.deleteEmployee} employees={this.state.employees} />
@@ -71,9 +72,23 @@ class ApplicationViews extends Component {
                 <Route path="/owners" render={(props) => {
                     return <OwnerList deleteOwner={this.deleteOwner} owners={this.state.owners} />
                 }} />
+                <Route path="/animals/:animalId(\d+)" render={(props) => {
+                    // Find the animal with the id of the route parameter
+                    let animal = this.state.animals.find(animal =>
+                        animal.id === parseInt(props.match.params.animalId)
+                    )
+
+                    // If the animal wasn't found, create a default one
+                    if (!animal) {
+                        animal = { id: 404, name: "404", breed: "Dog not found" }
+                    }
+
+                    return <AnimalDetail animal={animal}
+                        deleteAnimal={this.deleteAnimal} />
+                }} />
             </React.Fragment>
         )
     }
 }
 
-export default ApplicationViews
+export default withRouter(ApplicationViews)
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import dog from "./DogIcon.svg"
 import "./animal.css"
 
@@ -6,22 +7,23 @@ class AnimalList extends Component {
     render() {
         return (
             <section className="animals content">
-            <h3>List of Animals</h3>
-            {
-                this.props.animals.map(animal =>
-                    <div key={animal.id} className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <img src={dog} className="icon--dog" alt="dog"/>
-                                {animal.name}
-                                <button
-                                    onClick={() => this.props.deleteAnimal(animal.id)}
-                                    className="card-link">Delete</button>
-                            </h5>
+                <h3>List of Animals</h3>
+                {
+                    this.props.animals.map(animal =>
+                        <div key={animal.id} className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <img src={dog} className="icon--dog" alt="dog" />
+                                    {animal.name}
+                                    <Link className="nav-link" to={`/animals/${animal.id}`}>Details</Link>
+                                    <button
+                                        onClick={() => this.props.deleteAnimal(animal.id)}
+                                        className="card-link">Delete</button>
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                )
-            }
+                    )
+                }
             </section>
         )
     }
